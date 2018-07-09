9751d4e81d414f6a21341bf7240bc5320be0ef62
Marvin Do <marvin.do@everykey.com>
2018-07-09 10:10:02 -0700

    Initial commit

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Marvin Do on 7/9/18.
+//  Copyright © 2018 Marvin Do. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    //MARK :- Fields
+    @IBOutlet weak var messageLabel : UILabel!
+    @IBOutlet weak var latitudeLabel : UILabel!
+    @IBOutlet weak var longitudeLabel : UILabel!
+    @IBOutlet weak var addressLabel : UILabel!
+    @IBOutlet weak var tagButton : UIButton!
+    @IBOutlet weak var getButton : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getLocation() {
+        
+    }
+
+
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Marvin Do on 7/9/18.
+//  Copyright © 2018 Marvin Do. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
